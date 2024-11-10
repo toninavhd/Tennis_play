88877bc7bef032cd9ee9012d43ff8d5e45c2ed45
Antonio navarro hernandez <tillidie9999@gmail.com>
2024-11-10 14:49:59 +0000

    alguito pa empezar

--- play-tennis/main.py
+++ play-tennis/main.py
@@ -1,5 +1,32 @@
 def run(points: str) -> str:
-    # TODO
+    game_A = 0
+    game_B = 0
+    set_A = 0
+    set_B = 0
+    A_points = 0
+    B_points = 0
+    result= ''
+
+    for point in points:
+        if point == 'A':
+            A_points += 1
+        else:
+            B_points += 1
+
+# Aqui se van contando los juegos antes de que sea el fin del partido 
+
+        if A_points >= 4 and A_points - B_points >= 2:
+            game_A += 1
+            A_points = 0
+            B_points = 0
+        elif B_points >= 4 and B_points - A_points >= 2:
+            game_B += 1
+            A_points = 0
+            B_points = 0
+
+
+
+
     return result
 
 
