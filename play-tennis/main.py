7c85d79ab22ae8fd14f1e2a56c46bb8345c37183
toninavhd <tillidie9999@gmail.com>
2024-11-11 15:53:36 +0000

    cambie los nombres

--- play-tennis/main.py
+++ play-tennis/main.py
@@ -1,31 +1,27 @@
 def run(points: str) -> str:
-    game_A = 0
-    game_B = 0
+    game_a = 0
+    game_b = 0
     set_A = 0
     set_B = 0
-    A_points = 0
-    B_points = 0
-    result= ''
+    points_a = 0
+    points_b = 0
+    result = ''
 
     for point in points:
         if point == 'A':
-            A_points += 1
+            points_a += 1
         else:
-            B_points += 1
-# Aqui se van contando los juegos antes de que sea el fin del partido 
+            points_b += 1
+        # Aqui se van contando los juegos antes de que sea el fin del partido
 
-        if A_points >= 4 and A_points - B_points >= 2:
-            game_A += 1
-            A_points = 0
-            B_points = 0
-        elif B_points >= 4 and B_points - A_points >= 2:
-            game_B += 1
-            A_points = 0
-            B_points = 0
-
-# lo del B - A >= 2 es por si hay tail break y eso
-
-
+        if points_a >= 4 and points_a - points_b >= 2:
+            game_a += 1
+            points_a = 0
+            points_b = 0
+        elif points_b >= 4 and points_b - points_a >= 2:
+            game_b += 1
+            points_a = 0
+            points_b = 0
 
     return result
 
