cba8b8a97fddae0e009edc252c8c0c79c582602a
Antonio navarro hernandez <tillidie9999@gmail.com>
2024-11-11 19:13:31 +0000

    testing con lo que hay

--- play-tennis/main.py
+++ play-tennis/main.py
@@ -1,5 +1,6 @@
 def run(points: str) -> str:
     # Poner constantes para los números.
+    # Pista de Sergio: tailbreak es como un juego nuevo
 
     game_a = 0
     game_b = 0
@@ -14,7 +15,8 @@ def run(points: str) -> str:
             points_a += 1
         else:
             points_b += 1
-        # Aqui se van contando los juegos antes de que sea el fin del partido
+
+        # Aqui se van contando los juegos antes de que sea el fin del partido ↓
 
         if points_a >= 4 and points_a - points_b >= 2:
             game_a += 1
@@ -25,17 +27,23 @@ def run(points: str) -> str:
             points_a = 0
             points_b = 0
 
-    # pista de Sergio: tailbreak es como un juego nuevo
+        # Aqui se van contando los juegos ↓
+
         if game_a >= 6 and game_a - game_b >= 2:
             set_A += 1
-            result += f'{game_a} - {game_b}'
+            result += f'{game_a}-{game_b}'
             set_A = 0
             set_B = 0
         elif game_b >= 6 and game_b - game_a >= 2:
             set_B += 1
-            result += f'{game_a} - {game_b}'
+            result += f'{game_a}-{game_b}'
             set_A = 0
             set_B = 0
+        
+        # Aqui se marcan los resultados ↓
+
+        if game_a > 0 or game_b > 0:
+            result += f'{game_a}-{game_b}'
 
     return result
 
