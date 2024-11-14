3750798d5db2c2f8d4309805c28e4bb9c8d7ac14
Antonio navarro hernandez <tillidie9999@gmail.com>
2024-11-14 11:56:06 +0000

    main.py

--- play-tennis/main.py
+++ play-tennis/main.py
@@ -45,7 +45,7 @@ def run(points: str) -> str:
             game_a = 0
             game_b = 0
 
-    # Tiebreak (Esto no va)
+    # Tiebreak (Esto no va pero no se me ocurre nada mas)
         if game_a == SET_WIN_GAMES and game_b == SET_WIN_GAMES:
             points_a = 0
             points_b = 0
